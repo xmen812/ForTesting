cc23fa0c80f25debc48901cc57bf1b9083507e05
xmen812 <157883919+xmen812@users.noreply.github.com>
2024-01-27 15:52:10 +0530

    Initial commit

---
+++ src/test/java/MavenProject/Demo99/Login1.java
@@ -0,0 +1,13 @@
+package MavenProject.Demo99;
+
+import org.testng.annotations.Test;
+
+public class Login1 {
+	
+	System.setProperty("webdriver.chrome.driver", "D:\\3rdparty\\chrome\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+  @Test
+  public void f() {
+  }
+}
